663a00e759ae3626307934f1a3ee37d5b2742736
nishantb06 <nbhansali06@gmail.com>
2022-01-28 12:19:22 +0530

    wide mode added

--- streamapp-app.py
+++ streamapp-app.py
@@ -13,54 +13,67 @@ st.markdown ("A few links for referece - [1](https://dynamic-image-resizer.share
 st.markdown("[2](https://dynamic-image-resizer.sharechat.com/imageResizer/%E0%A4%95%E0%A5%83%E0%A4%B7%E0%A5%8D%E0%A4%A3%E0%A4%95%E0%A4%A5%E0%A4%BE%E0%A4%8F%E0%A4%82_35279ab8_1642535570715_sc_atrbtd.jpg) link for reference")
 URL_user = st.text_input("Image URL goes here")
 filter = st.selectbox("Which filter/Algorthm would you like to use",('Non Local Means Denoising Algorithm','Bilateral Filter',
-                                                                        'Gaussian Filter',"Median Filter","BM3D Filter"))
+                                                                        'Gaussian Filter',"Median Filter","BM3D Filter","All"))
 
 
-def NLMD(img):
+def NLMD(img,mode = "single"):
     
     h = st.slider("how powerful should the filter be(10 preferably)",1,100,10,1)
     dst = cv.fastNlMeansDenoisingColored(img,None,h,10,7,21)
 
-    col1,col2 = st.columns(2)
-    with col1:
-        st.image(image=img,caption = "before")
+    if mode == "single":
+        col1,col2 = st.columns(2)
+        with col1:
+            st.image(image=img,caption = "before")
 
-    with col2:
-        st.image(image=dst,caption = "after fast NL means denoising filter")
+        with col2:
+            st.image(image=dst,caption = "after fast NL means denoising filter")
+    
+    else:
+        return dst
     
 
-def BilateralFilter(img):
+def BilateralFilter(img,mode = "single"):
     dst = cv.bilateralFilter(img, 5, 10, 10)
 
-    col1,col2 = st.columns(2)
-    with col1:
-        st.image(image=img,caption = "before")
+    if mode == "single":
+        col1,col2 = st.columns(2)
+        with col1:
+            st.image(image=img,caption = "before")
 
-    with col2:
-        st.image(image=dst,caption = "after Bilateral filter")
+        with col2:
+            st.image(image=dst,caption = "after Bilateral filter")
+    else:
+        return dst
 
-def GaussianFilter(img):
+def GaussianFilter(img,mode = "single"):
     dst = cv.GaussianBlur(img, (9, 9), 0)
 
-    col1,col2 = st.columns(2)
-    with col1:
-        st.image(image=img,caption = "before")
+    if mode == "single":
+        col1,col2 = st.columns(2)
+        with col1:
+            st.image(image=img,caption = "before")
 
-    with col2:
-        st.image(image=dst,caption = "after Gaussian filter")
+        with col2:
+            st.image(image=dst,caption = "after Gaussian filter")
+    else:
+        return dst
 
 
-def MedianFilter(img):
+def MedianFilter(img,mode = "single"):
     dst = cv.medianBlur(img, 9)
 
-    col1,col2 = st.columns(2)
-    with col1:
-        st.image(image=img,caption = "before")
+    if mode == "single":
+        col1,col2 = st.columns(2)
+        with col1:
+            st.image(image=img,caption = "before")
 
-    with col2:
-        st.image(image=dst,caption = "after Gaussian filter")
+        with col2:
+            st.image(image=dst,caption = "after Gaussian filter")
+    else:
+        return dst
 
-def BM3DFilter(img):
+def BM3DFilter(img,mode = "single"):
     img = img_as_float(img)
     #dst = bm3d.bm3d(img,sigma_psd = 0.05,stage_arg=bm3d.BM3DStages.HARD_THRESHOLDING)
     # dst = np.clip(dst,0,1)
@@ -68,22 +81,42 @@ def BM3DFilter(img):
     sigma = st.slider("how powerful should the filter be(10 preferably)",min_value=0,max_value=100,value = 10,step=1)
     dst = bm3d.bm3d(img,sigma_psd = sigma/100,stage_arg=bm3d.BM3DStages.ALL_STAGES)
 
-    col1,col2 = st.columns(2)
-    with col1:
-        st.image(image=img,caption = "before")
+    if mode == "single":
+        col1,col2 = st.columns(2)
+        with col1:
+            st.image(image=img,caption = "before")
 
-    with col2:
-        st.image(image=dst,caption = "after BM3D filter",clamp  =True)
+        with col2:
+            st.image(image=dst,caption = "after BM3D filter",clamp  =True)
+    else:
+        return dst
 
     
 img = io.imread(URL_user)
 if filter == "Non Local Means Denoising Algorithm":
-    NLMD(img)
+    nlmd_img = NLMD(img)
 elif filter == "Bilateral Filter" :
-    BilateralFilter(img)
+    bilateral_img = BilateralFilter(img)
 elif filter == 'Gaussian Filter':
-    GaussianFilter(img)
+    gaussian_img = GaussianFilter(img)
 elif filter =="Median Filter":
-    MedianFilter(img)
+    median_img = MedianFilter(img)
 elif filter == "BM3D Filter":
-    BM3DFilter(img)
+    bm3d_img = BM3DFilter(img)
+elif filter == "All":
+    col1,col2,col3 = st.columns(3)
+    with col1:
+        st.image(img,caption = "original")
+        bilateral_img = BilateralFilter(img,mode = "double")
+        st.image(bilateral_img,caption = "bilateral")
+    with col2:
+        median_img = MedianFilter(img,mode = "double")
+        st.image(median_img,caption = "Median")
+        guassian_img = GaussianFilter(img,mode = "double")
+        st.image(guassian_img,caption  = "Gaussian")   
+    with col3:
+        bm3d_img = BM3DFilter(img,mode = "double")
+        st.image(bm3d_img,caption = "BM3d",clamp = True)
+        nlmd_img = NLMD(img,mode = "double")
+        st.image(nlmd_img,caption = "NLMD")
+    
